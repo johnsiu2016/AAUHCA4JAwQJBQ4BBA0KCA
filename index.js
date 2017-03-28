32863d86b4c97dac3bc8cb0dc455944adebe5c43
johnsiu2016 <buckyscode@gmail.com>
2017-03-29 01:25:00 +0800

    add jsdoc

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const co = require('co');
 const config = require('./config.json');
 const mongodb = require('./lib/mongodb');
 const Producer = require('./lib/producer');
+let producer = new Producer({
+	host: config.beanstalkd.host,
+	port: config.beanstalkd.port,
+	tube: config.beanstalkd.tube
+});
+const handler = require('./lib/currency-handler');
+let worker = new Worker({
+	host: config.beanstalkd.host,
+	port: config.beanstalkd.port,
+	tube: config.beanstalkd.tube,
+	handler: handler
+});
 
+// it is safer to establish the connection to mongodb and producer first.
 co(function* () {
 	yield mongodb({
 		host: config.mongodb.host
 	});
-
-	let producer = new Producer({
-		host: config.beanstalkd.host,
-		port: config.beanstalkd.port,
-		tube: config.beanstalkd.tube
-	});
 	yield producer.start();
-
-	let worker = new Worker({
-		host: config.beanstalkd.host,
-		port: config.beanstalkd.port,
-		tube: config.beanstalkd.tube,
-		handler: require('./lib/currency-handler')
-	});
 	worker.start();
 }).catch((err) => {
 	console.log(err.message);
--- seed.js
+++ seed.js
@@ -7,7 +7,7 @@ let host = config.beanstalkd.host;
 let port = config.beanstalkd.port;
 let tube = config.beanstalkd.tube;
 
-let joblist = [
+let joblist1 = [
 	{
 		from: 'USD',
 		to: 'HKD',
@@ -34,6 +34,16 @@ let joblist = [
 	}
 ];
 
+// let joblist1 = [
+// 	{
+// 		from: 'U123',
+// 		to: 'HKD',
+// 		success_count: 0,
+// 		failure_count: 0
+// 	}
+// ];
+
+
 let doneEmittingJobs = () => {
 	console.log('We reached our completion callback. Now closing down.');
 	producer.end();
@@ -42,17 +52,17 @@ let doneEmittingJobs = () => {
 
 let continuer = (err, jobid) => {
 	console.log('emitted job id: ' + jobid);
-	if (joblist.length === 0) {
+	if (joblist1.length === 0) {
 		return doneEmittingJobs();
 	}
 
-	return producer.put(0, 0, 60, JSON.stringify(joblist.shift()), continuer);
+	return producer.put(0, 0, 60, JSON.stringify(joblist1.shift()), continuer);
 };
 
 let producer = new fivebeans.client(host, port);
 producer.on('connect', () => {
 	producer.use(tube, (err, tname) => {
 		console.log('using' + tname);
-		producer.put(0, 0, 60, JSON.stringify(joblist.shift()), continuer);
+		producer.put(0, 0, 60, JSON.stringify(joblist1.shift()), continuer);
 	});
 }).connect();
